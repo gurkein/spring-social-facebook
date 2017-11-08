b5d7d323ab4286232d0730d16ca67d3a2838228f
Gürkan Vural <gurkan.vural@gmail.com>
2017-11-08 11:19:45 +0300

    - version 2.1.0.RC5 - post fields properties uncommented

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/FeedTemplate.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/FeedTemplate.java
@@ -291,7 +291,7 @@ class FeedTemplate implements FeedOperations {
 	private static final String[] ALL_POST_FIELDS = {
 			"id", "actions", "admin_creator", "application", "caption", "created_time", "description", "from{id,name,picture}", "icon",
 			"is_hidden", "is_published", "link", "message", "message_tags", "name", "object_id", "picture", "place", 
-			"privacy"/*, "properties"*/, "source", "status_type", "story", "to", "type", "updated_time", "with_tags", "shares",
+			"privacy", "properties", "source", "status_type", "story", "to", "type", "updated_time", "with_tags", "shares",
 			"attachments", "likes.limit(0).summary(1)", "comments.limit(0).summary(1)"
 	};
 
